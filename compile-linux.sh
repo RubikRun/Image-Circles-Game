75fa4678b427a5a7f7c04dfc1836f45a2c0d5851
RubikRun <borisd2000@gmail.com>
2020-06-15 10:38:22 +0300

    linux compile file update

--- compile-linux.sh
+++ compile-linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -O3 -o game main.cpp Game/Game.cpp Image/Circle.cpp Image/Image.cpp -l sfml-graphics -l sfml-window -l sfml-system
+g++ -O3 -o game main.cpp Game.cpp Circle.cpp Image.cpp -I SFML-2.5.1/include -L SFML-2.5.1/lib -l sfml-graphics -l sfml-window -l sfml-system
